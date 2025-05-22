f98ea7cd4e1518f11f4bccc97dbbe4e64cc9d32c
Piyush Kochar <piyushkochar909@gmail.com>
2025-05-22 16:36:34 +0530

    comment

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/<react-ecom-store>/', // <--- Add this line
+  base: '/<react-ecom-store>/', 
   build: {
     outDir: 'dist',
     sourcemap: false,
