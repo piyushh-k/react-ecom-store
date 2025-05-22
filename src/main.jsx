cd02fbf749016473685653b1b11ab19f2c4a556c
Piyush Kochar <piyushkochar909@gmail.com>
2025-05-22 16:53:05 +0530

    router

--- src/main.jsx
+++ src/main.jsx
@@ -10,7 +10,7 @@ import { Provider } from "react-redux";
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <Provider store={store}>
-      <BrowserRouter>
+      <BrowserRouter basename="/react-ecom-store/">
         <App />
       </BrowserRouter>
     </Provider>
