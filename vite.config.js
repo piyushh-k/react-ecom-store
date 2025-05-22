d80af0ba8bd80762389487e5c364f31edf9e2de5
Piyush Kochar <piyushkochar909@gmail.com>
2025-05-22 16:35:56 +0530

    dist

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,9 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/<react-ecom-store>/', // <--- Add this line
   build: {
     outDir: 'dist',
     sourcemap: false,
@@ -32,4 +32,4 @@ export default defineConfig({
       'Content-Security-Policy': "default-src 'self'; script-src 'self' 'unsafe-inline' 'unsafe-eval'; style-src 'self' 'unsafe-inline'; img-src 'self' data: https:; font-src 'self' data:; connect-src 'self' https://fakestoreapi.com https://formspree.io;"
     }
   }
-})
+});
