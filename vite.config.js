8608ae1fef109672df81a782cffd482abd5c7b2e
Piyush Kochar <piyushkochar909@gmail.com>
2025-07-12 13:16:40 +0530

    deploy

--- vite.config.js
+++ vite.config.js
@@ -4,9 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  // base: '/react-ecom-store/', // Removed for Vercel deployment
+  base: '/react-ecom-store/',
   build: {
-    outDir: 'dist', 
+    outDir: 'docs',
     sourcemap: false,
     minify: 'terser',
     terserOptions: {
