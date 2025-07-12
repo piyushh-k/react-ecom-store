d2a1b29751a1fcf81658543eab0b3c278ae85a10
Piyush Kochar <piyushkochar909@gmail.com>
2025-07-12 13:12:48 +0530

    fix: remove base path for Vercel deployment

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/react-ecom-store/', 
+  // base: '/react-ecom-store/', // Removed for Vercel deployment
   build: {
     outDir: 'dist', 
     sourcemap: false,
